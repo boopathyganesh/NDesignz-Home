8d3f187cac325b325241f7da97b9d9e54dfe6c22
boopathyganesh <boopathyganesh95@gmail.com>
2024-09-01 20:37:18 +0530

    gallery update

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,28 +5,52 @@ import { Parallax } from "react-scroll-parallax";
 export default function Home() {
   return (
     <main className="overflow-hidden text-gray-700 ">
-      <div className="flex items-center justify-center px-5 py-2 mx-auto lg:pt-24 lg:px-32 mt-10">
+      <div className="flex flex-col items-center justify-center gap-4 px-5 py-2 mx-auto lg:pt-24 lg:px-32 mt-10">
         <div className="flex flex-wrap -m-1 md:-m-2">
           <div className="flex flex-wrap w-1/2">
             <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/web_asset2.jpg" height={500} width={500} />
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset2.jpg" height={500} width={500} />
             </div>
             <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/web_asset2.jpg" height={500} width={500} />
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset1.jpg" height={500} width={500} />
             </div>
             <div className="w-full p-1 md:p-2">
-              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/web_asset1.jpg" height={500} width={500} />
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset3.jpg" height={500} width={500} />
             </div>
           </div>
           <div className="flex flex-wrap w-1/2">
             <div className="w-full p-1 md:p-2">
-              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/web_asset2.jpg" height={500} width={500} />
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset4.jpg" height={500} width={500} />
             </div>
             <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/web_asset3.jpg" height={500} width={500} />
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset5.jpg" height={500} width={500} />
             </div>
             <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/web_asset2.jpg" height={500} width={500} />
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset6.jpg" height={500} width={500} />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap -m-1 md:-m-2">
+          <div className="flex flex-wrap w-1/2">
+            <div className="w-1/2 p-1 md:p-2">
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset2.jpg" height={500} width={500} />
+            </div>
+            <div className="w-1/2 p-1 md:p-2">
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset1.jpg" height={500} width={500} />
+            </div>
+            <div className="w-full p-1 md:p-2">
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset3.jpg" height={500} width={500} />
+            </div>
+          </div>
+          <div className="flex flex-wrap w-1/2">
+            <div className="w-full p-1 md:p-2">
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset4.jpg" height={500} width={500} />
+            </div>
+            <div className="w-1/2 p-1 md:p-2">
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset5.jpg" height={500} width={500} />
+            </div>
+            <div className="w-1/2 p-1 md:p-2">
+              <Image alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset6.jpg" height={500} width={500} />
             </div>
           </div>
         </div>
--- components/OurExpertise.tsx
+++ components/OurExpertise.tsx
@@ -8,21 +8,20 @@ import { Parallax } from 'react-scroll-parallax'
 
 const OurExpertise = () => {
   return (
-    <Parallax scale={[0, 1.3, 'easeOut']}>
-      <section className='my-10 p-5 max-w-7xl w-full bg-black text-white'>
-        <div className='flex flex-col items-center justify-center'>
-          <div className='flex flex-col items-center justify-center text-center'>
-            <h1 className='text-4xl font-semibold text-gold-500 mb-3'>Why should you Consider Us?</h1>
-            <p className='text-lg font-light text-white/70'>We Work To Make Your Interior Lifestyle Dreams Come True. Be It Your Home, Office, Commercial, Residential, Industry, Or Public Space, Everyone Has Ideal Thoughts In Mind. No Mess! For Our Customers, Our Home Interiors Bring That Essence To Interior Lifestyle.</p>
-          </div>
-          <div className='mt-10 flex flex-wrap items-center justify-center gap-4'>
-            {expertise.map((item, index) => (
-              <MiniCard key={index} title={item.title} icon={item.icon} content={item.content} />
-            ))}
-          </div>
+    <section className='my-10 p-5 max-w-7xl w-full bg-black text-white'>
+      <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center text-center'>
+          <h1 className='text-4xl font-semibold text-gold-500 mb-3'>Why should you Consider Us?</h1>
+          <p className='text-lg font-light text-white/70'>We Work To Make Your Interior Lifestyle Dreams Come True. Be It Your Home, Office, Commercial, Residential, Industry, Or Public Space, Everyone Has Ideal Thoughts In Mind. No Mess! For Our Customers, Our Home Interiors Bring That Essence To Interior Lifestyle.</p>
         </div>
-      </section>
-    </Parallax>
+        <div className='mt-10 flex flex-wrap items-center justify-center gap-4'>
+          {expertise.map((item, index) => (
+            <MiniCard key={index} title={item.title} icon={item.icon} content={item.content} />
+          ))}
+        </div>
+      </div>
+    </section>
+
   )
 }
 
