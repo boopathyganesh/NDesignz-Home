57d8f8976b973eb913e92b7031fb21485a48e5b7
boopathyganesh <boopathyganesh95@gmail.com>
2024-11-21 15:18:16 +0530

    number scrolling + gallery update

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,61 +1,44 @@
-"use client"
+"use client";
 import Image from "next/image";
-import { Parallax } from "react-scroll-parallax";
 
-export default function Home() {
+const imageLinks = [
+  "/images/assets/asset1.jpg",
+  "/images/assets/asset2.jpg",
+  "/images/assets/asset3.jpg",
+  "/images/assets/asset4.jpg",
+  "/images/assets/asset5.jpg",
+  "/images/assets/asset6.jpg",
+  "/images/assets/asset1.jpg",
+  "/images/assets/asset2.jpg",
+  "/images/assets/asset3.jpg",
+  "/images/assets/asset4.jpg",
+  "/images/assets/asset5.jpg",
+  "/images/assets/asset6.jpg",
+];
+
+export default function Gallery() {
   return (
-    <main className="overflow-hidden text-gray-700 ">
+    <main className="overflow-hidden text-gray-700">
       <div className="flex flex-col items-center justify-center gap-4 px-5 py-2 mx-auto lg:pt-24 lg:px-32 mt-32">
         <div className="flex items-center justify-center text-center">
           <h1 className="text-3xl text-teal-700 font-semibold">Our Precious Gallery</h1>
         </div>
         <div className="flex flex-wrap -m-1 md:-m-2">
-          <div className="flex flex-wrap w-full md:w-1/2">
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset2.jpg" height={500} width={500} />
+          {imageLinks.map((src, index) => (
+            <div
+              key={index}
+              className={`p-1 md:p-2 ${index % 3 === 0 ? "w-1/2" : "w-1/4"
+                }`}
+            >
+              <Image
+                alt={`gallery-${index}`}
+                className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg"
+                src={src}
+                height={500}
+                width={500}
+              />
             </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset1.jpg" height={500} width={500} />
-            </div>
-            <div className="w-full p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset3.jpg" height={500} width={500} />
-            </div>
-          </div>
-          <div className="flex flex-wrap w-full md:w-1/2">
-            <div className="w-full p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset4.jpg" height={500} width={500} />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset5.jpg" height={500} width={500} />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset6.jpg" height={500} width={500} />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-wrap -m-1 md:-m-2">
-          <div className="flex flex-wrap w-full md:w-1/2">
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset2.jpg" height={500} width={500} />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset1.jpg" height={500} width={500} />
-            </div>
-            <div className="w-full p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset3.jpg" height={500} width={500} />
-            </div>
-          </div>
-          <div className="flex flex-wrap w-full md:w-1/2">
-            <div className="w-full p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset4.jpg" height={500} width={500} />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset5.jpg" height={500} width={500} />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image alt="gallery" className="block hover:scale-105 smooth object-cover object-center w-full h-full rounded-lg" src="/images/assets/asset6.jpg" height={500} width={500} />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </main>
--- components/OurAchievements.tsx
+++ components/OurAchievements.tsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image'
+import useScrollTriggeredCountUp from '@/hooks/useScrollTriggeredCountUp'
 
 
 const OurAchievements = () => {
+  const counterRef = useRef<HTMLDivElement>(null)
   return (
     <section className='max-w-sm md:max-w-4xl lg:max-w-4xl mb-10 md:p-5 w-full bg-white text-black'>
       <div className='flex items-center justify-center gap-1'>
         <div className='w-full h-[600px] md:h-[700px] p-5 flex flex-col items-start justify-between gap-10 group'>
-          <div className='flex items-center justify-between w-full md:px-5'>
+          <div className='flex items-center justify-between w-full md:px-5' ref={counterRef}>
             <div className='text-left font-medium text-base md:text-lg text-black'>
-              <h1 className='text-teal-700 text-2xl md:text-4xl font-bold'>5+</h1>
+              <h1 className='text-teal-700 text-2xl md:text-4xl font-bold'>{useScrollTriggeredCountUp(counterRef, 5, 3000)}+</h1>
               <h2>Years of Experience</h2>
             </div>
             <div className='text-center font-medium text-base md:text-lg text-black'>
-              <h1 className='text-teal-700 text-2xl md:text-4xl font-bold'>100+</h1>
+              <h1 className='text-teal-700 text-2xl md:text-4xl font-bold'>{useScrollTriggeredCountUp(counterRef, 100, 3000)}+</h1>
               <h2>Interior Projects</h2>
             </div>
             <div className='text-right font-medium text-base md:text-lg text-black'>
-              <h1 className='text-teal-700 text-2xl md:text-4xl font-bold'>100+</h1>
+              <h1 className='text-teal-700 text-2xl md:text-4xl font-bold'>{useScrollTriggeredCountUp(counterRef, 100, 3000)}+</h1>
               <h2>Happy Clients</h2>
             </div>
           </div>
           <div className='h-96 w-full overflow-hidden flex items-center' >
-            <Image src={'/images/assets/asset8.jpg'} width={500} height={500} alt={''} className='w-full group-hover:scale-105 md:group-hover:rotate-6 smooth' />
+            <Image src={'/images/assets/asset9.jpg'} width={500} height={500} alt={''} className='w-full group-hover:scale-110 smooth' />
           </div>
           <div>
             <p className='text-sm md:text-base text-center'>Choosing Livstyle Homes means choosing a partner who genuinely cares about bringing your vision to life. Our team combines technical expertise with creative flair, delivering spaces that are beautiful, practical, and built to last. We take pride in our ability to translate ideas into reality, paying close attention to each client’s preferences while adding our unique touch.</p>
