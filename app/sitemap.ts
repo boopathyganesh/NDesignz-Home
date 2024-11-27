40e3f5f5903c1259c4c84f7ca1531ddcc346c39f
boopathyganesh <boopathyganesh95@gmail.com>
2024-11-27 13:06:04 +0530

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,30 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://livstylehomes.com/',
-            lastModified: new Date(),
+            lastModified: new Date('2024-11-27'),
             changeFrequency: 'monthly',
             priority: 1,
         },
         {
             url: 'https://livstylehomes.com/about',
-            lastModified: new Date(),
+            lastModified: new Date('2024-11-27'),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
             url: 'https://livstylehomes.com/contact',
-            lastModified: new Date(),
+            lastModified: new Date('2024-11-27'),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
             url: 'https://livstylehomes.com/gallery',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
+            lastModified: new Date('2024-11-27'),
+            changeFrequency: 'weekly',
             priority: 1,
         },
-    ]
+    ];
 }
